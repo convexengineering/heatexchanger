711886c399bbdb683c7cde6a60ad603651ca55dc
1ozturkbe <1ozturkbe@gmail.com>
2018-03-24 14:57:23 -0400

    Add max dims and AR.

--- heatexchanger/layer.py
+++ heatexchanger/layer.py
@@ -19,7 +19,10 @@ class Layer(Model):
     V_tot           [cm^3]    total volume
     V_mtrl          [cm^3]    volume of material
     g          9.81 [m*s^-2]  gravitational acceleration
-
+    x_dim      5    [cm]      max hot length
+    y_dim      5    [cm]      max cold length
+    z_dim      1    [cm]      max height
+    maxAR      4    [-]       max aspect ratio of tiles
 
     Upper Unbounded
     ---------------
@@ -40,13 +43,13 @@ class Layer(Model):
         with Vectorize(Nairpipes):
             airpipes = RectangularPipe(Nwaterpipes, air, increasingT=True,
                                        substitutions={"T_in": 303,
-                                                      "v_in": 20})
+                                                      "v_in": 10})
             self.airpipes = airpipes
         water = Water()
         with Vectorize(Nwaterpipes):
             waterpipes = RectangularPipe(Nairpipes, water, increasingT=False,
                                          substitutions={"T_in": 500,
-                                                        "v_in": 5})
+                                                        "v_in": 1})
             self.waterpipes = waterpipes
 
         with Vectorize(Nwaterpipes):
@@ -71,23 +74,39 @@ class Layer(Model):
         # Linking pipes in c
         for i in range(Nwaterpipes):
             for j in range(Nairpipes):
+                geom.extend([c.x_cell[i,j] == waterpipes.w[i],
+                             c.x_cell[i,j] == airpipes.l_seg[j,i],
+                             c.y_cell[i,j] == airpipes.w[j],
+                             c.y_cell[i,j] == waterpipes.l_seg[i,j],
+                             maxAR >= c.x_cell[i,j]/c.y_cell[i,j],
+                             maxAR >= c.y_cell[i,j]/c.x_cell[i,j],
+                             # Arbitrary bounding for convergence.
+                             c.x_cell[i,j] >= 0.2*units('cm'),
+                             c.y_cell[i,j] >= 0.2*units('cm'),
+                             ])
+
+        # Linking pipes in c
+        for i in range(Nwaterpipes):
+            for j in range(Nairpipes):
                 geom.extend([
-                    c.x_cell[i,j] == waterpipes.w[i],
-                    c.x_cell[i,j] == airpipes.l_seg[j,i],
-                    c.y_cell[i,j] == airpipes.w[j],
-                    c.y_cell[i,j] == waterpipes.l_seg[i,j],
-                    c.dQ[i,j]     == waterpipes.dQ[i,j],
-                    c.dQ[i,j]     == airpipes.dQ[j,i],
-                    c.Tr_hot[i,j] == waterpipes.Tr_int[i,j],
-                    c.Tr_cld[i,j] == airpipes.Tr_int[j,i],
-                    c.T_hot[i,j]  == waterpipes.T_avg[i,j],
-                    c.T_cld[i,j]  == airpipes.T_avg[j,i],
-                    c.h_hot[i,j]  == waterpipes.h[i,j],
-                    c.h_cld[i,j]  == airpipes.h[j,i],
-                    c.z_hot[i,j]  == waterpipes.h_seg[i,j],
-                    c.z_cld[i,j]  == airpipes.h_seg[j,i],
-                    c.t_hot[i,j]  >= 0.05/((i+1.)**3*(j+1.)**3.)**(1./3.)*units('cm'),
-                    c.t_cld[i,j]  >= 0.05/((i+1.)**3*(j+1.)**3.)**(1./3.)*units('cm'),
+                        c.dQ[i,j]     == waterpipes.dQ[i,j],
+                        c.dQ[i,j]     == airpipes.dQ[j,i],
+                        c.Tr_hot[i,j] == waterpipes.Tr_int[i,j],
+                        c.Tr_cld[i,j] == airpipes.Tr_int[j,i],
+                        c.T_hot[i,j]  == waterpipes.T_avg[i,j],
+                        c.T_cld[i,j]  == airpipes.T_avg[j,i],
+                        c.h_hot[i,j]  == waterpipes.h[i,j],
+                        c.h_cld[i,j]  == airpipes.h[j,i],
+                        c.z_hot[i,j]  == waterpipes.h_seg[i,j],
+                        c.z_cld[i,j]  == airpipes.h_seg[j,i],
+                        c.t_hot[i,j]     >= 0.05/((i+1.)**3*(j+1.)**3.)**(1./3.)*units('cm'),
+                        c.t_cld[i,j]     >= 0.05/((i+1.)**3*(j+1.)**3.)**(1./3.)*units('cm'),
+                        waterpipes.h_seg[i,j] >= 0.1*units('cm'),
+                        waterpipes.h_seg[i,j] <= 0.2*units('cm'),
+                        airpipes.h_seg[j,i] >= 0.2*units('cm'),
+                        airpipes.h_seg[j,i] <= 1.0*units('cm'),
+                        x_dim >= waterpipes.w.sum(),
+                        y_dim >= airpipes.w.sum(),
                     ])
 
         return [
@@ -108,9 +127,10 @@ class Layer(Model):
             airCf,
 
             # HEAT EXCHANGE REQUIREMENT
+            #waterpipes.T[:,-1] <= 400*units('K'),
 
             # TOTAL VOLUME REQUIREMENT
-            V_tot <= 2*units('cm^3'),
+            V_tot <= 50*units('cm^3'),
 
             # MATERIAL VOLUME
             V_mtrl >= (c.z_hot*c.t_hot*c.x_cell).sum()+(c.z_cld*c.t_cld*c.y_cell).sum()+(c.x_cell*c.y_cell*c.t_plate).sum(),
@@ -126,7 +146,7 @@ if __name__ == "__main__":
     m.cost = (m.D_air+m.D_wat)/m.Q
     #m = Model(m.cost,Bounded(m))
     #m = relaxed_constants(m)
-    sol = m.localsolve(verbosity=0)
+    sol = m.localsolve(verbosity=2)
     #post_process(sol)
     print sol('Q')
     print sol('eta_h')
