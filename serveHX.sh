62d0d798161bb5264329a98d886555911822ae83
Edward Burnell <eburn@mit.edu>
2018-04-04 00:26:09 -0700

    add single-serving script

---
+++ serveHX.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd heatexchanger
+python server.py > server.log &
+while [ ! -f sol.txt ]; do sleep 1; done  # poll every second
+serveCSM HX.csm  > csm.log &
+while [ ! -f HX.egads ]; do sleep 1; done  # poll every second
+python -m webbrowser -t ../ESP/ESP-localhost7681.html
+
