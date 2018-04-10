d44e10f5dec57e3de3ae3e42273bf094f02a1ed7
Edward Burnell <eburn@mit.edu>
2018-04-10 15:53:05 -0700

    explicit units in gencsm

--- heatexchanger/gencsm.py
+++ heatexchanger/gencsm.py
@@ -11,17 +11,17 @@ def gencsm(m, sol, ID):
     nParams = 8
 
     # Creating corner coordinates
-    x = [sum(sol(m.hotpipes.w)[0:i].magnitude) for i in range(nu+1)]
-    y = [sum(sol(m.coldpipes.w)[0:i].magnitude) for i in range(nw+1)]
+    x = [sum(sol(m.hotpipes.w)[0:i].to("m").magnitude) for i in range(nu+1)]
+    y = [sum(sol(m.coldpipes.w)[0:i].to("m").magnitude) for i in range(nw+1)]
     xy = np.array([(x[i], y[j]) for j in range(nw+1) for i in range(nu+1)])
     xycent = np.array([[(x[i+1] + x[i])/2, (y[j+1]+y[j])/2]
                        for j in range(nw) for i in range(nu)])
     z = sol(m.c.z_hot) + sol(m.c.z_cld) + 2*sol(m.c.t_plate)
 
-    hxVals = [sol(m.c.t_plate).magnitude,
-              sol(m.c.t_hot).magnitude,
-              sol(m.c.t_cld).magnitude,
-              sol(m.c.z_hot).magnitude/sol(m.c.z_cld).magnitude]
+    hxVals = [10*sol(m.c.t_plate).to("m").magnitude,
+              10*sol(m.c.t_hot).to("m").magnitude,
+              10*sol(m.c.t_cld).to("m").magnitude,
+              (sol(m.c.z_hot)/sol(m.c.z_cld)).magnitude]
     intlist = [interp2d(xycent[:, 0], xycent[:, 1], hxVals[i], kind='linear')
                for i in range(len(hxVals))]
 
