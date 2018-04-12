d60dee0c29d40c8af4530f66d2129ba6043aab4a
1ozturkbe <1ozturkbe@gmail.com>
2018-04-12 17:10:17 -0400

    Finally getting a hang of the innermost,leftmost rule. Fixed asymmetric number of cells issue.

--- heatexchanger/designHX.py
+++ heatexchanger/designHX.py
@@ -16,13 +16,17 @@ from writetotext import genHXData
 
 # Initializing SP single-layer HX model
 imp.reload(layer)
-Ncold, Nhot = 5, 5
-m = layer.Layer(Ncold, Nhot, Air(), Water(), StainlessSteel())
+Ncoldpipes, Nhotpipes = 5, 4
+coldFluid = Air()
+hotFluid = Water()
+material = StainlessSteel()
+n_fins = 5
+m = layer.Layer(Ncoldpipes, Nhotpipes, coldFluid, hotFluid, material)
 
 # Model input parameters
 m.substitutions.update({
                         # Geometric parameters,
-                        m.n_fins:      5.,
+                        m.n_fins:      n_fins,
                         m.x_dim:       5.*units('cm'),
                         m.y_dim:       10.*units('cm'),
                         m.z_dim:       1.*units('cm')
@@ -39,7 +43,7 @@ sol = m.localsolve(verbosity=2)
 print sol('Q')
 
 # Generating 2D plots
-gen_plots(m, sol, Ncold, Nhot)
+gen_plots(m, sol, Ncoldpipes, Nhotpipes)
 
 # Writing complete solution file sol.txt
 with open("sol.txt", "w") as f:
--- heatexchanger/layer.py
+++ heatexchanger/layer.py
@@ -73,21 +73,22 @@ class Layer(Model):
 
         geom = [V_tot >= sum(sum(hotpipes.V_seg)) + sum(sum(coldpipes.V_seg)) + V_mtrl]
 
+        # REMEMBER: LEFTMOST, INNERMOST RULE OF VECTORIZATION
         for i in range(Nhotpipes):
             for j in range(Ncoldpipes):
-                print c.x_cell[i,j]
-                geom.extend([c.x_cell[i,j] == hotpipes.w[i],
-                             c.x_cell[i,j] == coldpipes.l_seg[j,i],
-                             c.y_cell[i,j] == coldpipes.w[j],
-                             c.y_cell[i,j] == hotpipes.l_seg[i,j],
-                             maxAR >= c.y_cell[i,j]/c.x_cell[i,j],
-                             maxAR >= c.x_cell[i,j]/c.y_cell[i,j],
+                print c.x_cell[j,i]
+                geom.extend([c.x_cell[j,i] == hotpipes.w[i],
+                             c.x_cell[j,i] == coldpipes.l_seg[i,j],
+                             c.y_cell[j,i] == coldpipes.w[j],
+                             c.y_cell[j,i] == hotpipes.l_seg[j,i],
+                             maxAR >= c.y_cell[j,i]/c.x_cell[j,i],
+                             maxAR >= c.x_cell[j,i]/c.y_cell[j,i],
                              # Arbitrary bounding for convergence.
-                             c.x_cell[i,j] >= 0.1*units('cm'),
-                             c.y_cell[i,j] >= 0.1*units('cm'),
+                             c.x_cell[j,i] >= 0.1*units('cm'),
+                             c.y_cell[j,i] >= 0.1*units('cm'),
                              # Differentiating between flow width and cell width
-                             c.x_cell[i,j] >= n_fins*(c.t_hot[i,j] + hotpipes.w_fluid[i,j]),
-                             c.y_cell[i,j] >= n_fins*(c.t_cld[i,j] + coldpipes.w_fluid[i,j]),
+                             c.x_cell[j,i] >= n_fins*(c.t_hot[j,i] + hotpipes.w_fluid[j,i]),
+                             c.y_cell[j,i] >= n_fins*(c.t_cld[j,i] + coldpipes.w_fluid[i,j]),
                              # Making sure there is at least 1 (non-integer) number of fins
                              n_fins >= 1.,
                              ])
@@ -96,20 +97,20 @@ class Layer(Model):
         for i in range(Nhotpipes):
             for j in range(Ncoldpipes):
                 geom.extend([
-                        c.dQ[i,j]     == hotpipes.dQ[i,j],
-                        c.dQ[i,j]     == coldpipes.dQ[j,i],
-                        c.Tr_hot[i,j] == hotpipes.Tr_int[i,j],
-                        c.Tr_cld[i,j] == coldpipes.Tr_int[j,i],
-                        c.T_hot[i,j]  == hotpipes.T_avg[i,j],
-                        c.T_cld[i,j]  == coldpipes.T_avg[j,i],
-                        c.h_hot[i,j]  == hotpipes.h[i,j],
-                        c.h_cld[i,j]  == coldpipes.h[j,i],
-                        c.z_hot[i,j]  == hotpipes.h_seg[i,j],
-                        c.z_cld[i,j]  == coldpipes.h_seg[j,i],
-                        hotpipes.h_seg[i,j] >= 0.1*units('cm'),
-                        hotpipes.h_seg[i,j] <= 1.0*units('cm'),
-                        coldpipes.h_seg[j,i] >= 0.1*units('cm'),
-                        coldpipes.h_seg[j,i] <= 1.0*units('cm'),
+                        c.dQ[j,i]     == hotpipes.dQ[j,i],
+                        c.dQ[j,i]     == coldpipes.dQ[i,j],
+                        c.Tr_hot[j,i] == hotpipes.Tr_int[j,i],
+                        c.Tr_cld[j,i] == coldpipes.Tr_int[i,j],
+                        c.T_hot[j,i]  == hotpipes.T_avg[j,i],
+                        c.T_cld[j,i]  == coldpipes.T_avg[i,j],
+                        c.h_hot[j,i]  == hotpipes.h[j,i],
+                        c.h_cld[j,i]  == coldpipes.h[i,j],
+                        c.z_hot[j,i]  == hotpipes.h_seg[j,i],
+                        c.z_cld[j,i]  == coldpipes.h_seg[i,j],
+                        hotpipes.h_seg[j,i] >= 0.1*units('cm'),
+                        hotpipes.h_seg[j,i] <= 1.0*units('cm'),
+                        coldpipes.h_seg[i,j] >= 0.1*units('cm'),
+                        coldpipes.h_seg[i,j] <= 1.0*units('cm'),
                         x_dim >= hotpipes.w.sum(),
                         y_dim >= coldpipes.w.sum(),
                         z_dim >= c.z_hot+c.z_cld+c.t_plate,
